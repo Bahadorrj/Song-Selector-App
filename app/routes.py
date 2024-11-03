d8a1f7f60f86267c77a2487de00b3c1c3019eff1
Bahador Rousta <bahador.jorshary@gmail.com>
2024-11-04 00:19:04 +0330

    Add responses to routes

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import sqlite3
 import mimetypes
 from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify
@@ -58,3 +59,29 @@ def success():
 
     # If someone tries to access success page directly, redirect to index
     return redirect(url_for("main.index"))
+
+
+@bp.route("/responses", methods=["GET"])
+def get_responses():
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM Responds")
+    responds = cursor.fetchall()
+    responds_data = [dict(row) for row in responds]
+
+    cursor.execute("SELECT * FROM Songs")
+    songs = cursor.fetchall()
+    songs_data = [dict(row) for row in songs]
+
+    conn.close()
+
+    counter = defaultdict(int)
+    for respond in responds_data:
+        key = next(
+            song_data
+            for song_data in songs_data
+            if song_data["id"] == respond["song_id"]
+        ).get("title")
+        counter[key] += 1
+    return jsonify(counter)
